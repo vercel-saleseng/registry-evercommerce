a5e21d7a7215584881a2591f3c823e90072844e0
Derek Wu <derekwu35@gmail.com>
2025-05-19 12:21:53 -0400

    updated with customerlobby logo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '9xunyj9nbtcqjdrd.public.blob.vercel-storage.com',
+            },
+        ],
+    },
+};
 
 const withVercelToolbar = require("@vercel/toolbar/plugins/next")();
 export default withVercelToolbar(nextConfig);
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,22 +1,15 @@
+import Image from "next/image";
+
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="size-8"
-        role="graphics-symbol"
-      >
-        <circle className="fill-primary" cx="13.5" cy="6.5" r="2.5" />
-        <circle className="fill-primary" cx="19" cy="13" r="2.5" />
-        <circle className="fill-primary" cx="13.5" cy="19.5" r="2.5" />
-        <circle className="fill-primary" cx="6.5" cy="13" r="2.5" />
-      </svg>
-
-      <h1 className="font-bold">Brand Name</h1>
+      <Image
+        height={32}
+        width={120}
+        src="https://9xunyj9nbtcqjdrd.public.blob.vercel-storage.com/evercommerce-logo-zR7uxFGhnPtYvJagtTDJP5bLUyFsQx"
+        alt="Logo"
+        className="px-2 py-4"
+      />
     </div>
   );
 }
